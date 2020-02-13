bff7731b40377bfed990fb83b0a8b25fc26d8ace
Lucien Greathouse <me@lpghatguy.com>
2020-02-12 17:55:41 -0800

    Populate ~/.foreman/foreman.toml automatically. Closes #1.

--- src/paths.rs
+++ src/paths.rs
@@ -2,6 +2,8 @@
 
 use std::{fs, io, path::PathBuf};
 
+static DEFAULT_USER_CONFIG: &str = include_str!("../resources/default-foreman.toml");
+
 pub fn base_dir() -> PathBuf {
     let mut dir = dirs::home_dir().unwrap();
     dir.push(".foreman");
@@ -30,6 +32,15 @@ pub fn create() -> io::Result<()> {
     fs::create_dir_all(base_dir())?;
     fs::create_dir_all(bin_dir())?;
     fs::create_dir_all(tools_dir())?;
+
+    let config = user_config();
+    if let Err(err) = fs::metadata(&config) {
+        if err.kind() == io::ErrorKind::NotFound {
+            fs::write(&config, DEFAULT_USER_CONFIG)?;
+        } else {
+            return Err(err);
+        }
+    }
 
     Ok(())
 }
