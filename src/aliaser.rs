983bbfcb4f686468c4f32c62938a9e310cb025dd
Lucien Greathouse <me@lpghatguy.com>
2020-02-14 17:15:17 -0800

    Fix use of env::args() to use env::current_exe()

--- src/aliaser.rs
+++ src/aliaser.rs
@@ -3,7 +3,7 @@ use std::env::{self, consts::EXE_SUFFIX};
 use crate::{fs, paths};
 
 pub fn add_self_alias(name: &str) {
-    let foreman_path = env::args().next().unwrap();
+    let foreman_path = env::current_exe().unwrap();
     let mut alias_path = paths::bin_dir();
     alias_path.push(format!("{}{}", name, EXE_SUFFIX));
 
