4b63fe4bbbdb3633066f1b8b4d8146b284e84cd7
Matt Hargett <plaztiksyke@gmail.com>
2021-11-13 13:26:15 -0800

    Add ability to increase logging level from the commandline. Default to 'info' level. Add a log message explaining when a release version tag name isn't being used.

--- src/tool_cache.rs
+++ src/tool_cache.rs
@@ -80,6 +80,7 @@ impl ToolCache {
 
                 let version = Version::parse(&release.tag_name).ok().or_else(|| {
                     if !release.tag_name.starts_with('v') {
+                	log::info!("Release tag name did not start with 'v'! {}", release.tag_name);
                         return None;
                     }
 
