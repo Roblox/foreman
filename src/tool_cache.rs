b9d90bf03cd88452837df17c90c8bd6a9658b0da
Matt Hargett <plaztiksyke@gmail.com>
2021-11-13 13:44:09 -0800

    Clippy and bump rust versions we test with

--- src/tool_cache.rs
+++ src/tool_cache.rs
@@ -80,7 +80,10 @@ impl ToolCache {
 
                 let version = Version::parse(&release.tag_name).ok().or_else(|| {
                     if !release.tag_name.starts_with('v') {
-                	log::info!("Release tag name did not start with 'v'! {}", release.tag_name);
+                	log::info!(
+                            "Release tag name did not start with 'v'! {}", 
+                            release.tag_name
+                        );
                         return None;
                     }
 
