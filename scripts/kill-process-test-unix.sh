086b068316e5b3b769af197da19ea784e89855d0
Aiden Fujiwara <afujiwara@roblox.com>
2023-10-04 14:59:01 -0700

    test runinng rojo serve and killing

---
+++ scripts/kill-process-test-unix.sh
@@ -0,0 +1,48 @@
+write_foreman_toml () {
+    echo "[tools]" > foreman.toml
+    echo "$2 = { $1 = \"$3\", version = \"=$4\" }" >> foreman.toml
+}
+
+create_rojo_files() {
+    echo "{
+        \"name\": \"test\",
+        \"tree\": {
+            \"\$path\": \"src\"
+            }
+    }" > default.project.json
+}
+
+setup_rojo() {
+    write_foreman_toml github rojo "rojo-rbx/rojo" "7.3.0"
+    cargo run --release -- install
+    create_rojo_files
+}
+
+delay_kill_process_and_check() {
+    sleep 5
+    echo "waiting 5 seconds before killing rojo"
+    ps -ef | grep "rojo serve" | grep -v grep | awk '{print $2}' | xargs kill -INT
+    check_killed_subprocess
+}
+
+run_rojo_serve_and_kill_process() {
+    setup_rojo
+    (rojo serve default.project.json) & (delay_kill_process_and_check)
+}
+
+check_killed_subprocess(){
+    if ps -ef | grep "rojo" | grep -v grep 
+    then 
+        echo "rojo subprocess was not killed properly"
+        rm foreman.toml
+        rm default.project.json
+        exit 1
+    else
+        echo "rojo subprocess was killed properly"
+        rm foreman.toml
+        rm default.project.json
+        exit 0
+    fi
+}
+
+run_rojo_serve_and_kill_process
