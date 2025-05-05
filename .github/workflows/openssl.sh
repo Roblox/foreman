9a981191e6dc2f5f809df645ce2e2ddef6ce8d9e
Nick Winans <nick@winans.io>
2025-05-05 13:56:54 -0700

    Add L flag to curl download of openssl

--- .github/workflows/openssl.sh
+++ .github/workflows/openssl.sh
@@ -7,7 +7,7 @@ OPENSSL_VER=${OPENSSL_VER:-openssl-1.1.1n}
 OPENSSL_DST=${PWD}/${OPENSSL_VER}-install
 
 if [[ ! -d ${OPENSSL_DST} ]]; then
-    curl -O https://www.openssl.org/source/${OPENSSL_VER}.tar.gz
+    curl -OL https://www.openssl.org/source/${OPENSSL_VER}.tar.gz
     tar xzf ${OPENSSL_VER}.tar.gz
     cd ${OPENSSL_VER}
     ./Configure no-shared enable-rc5 zlib darwin64-arm64-cc --prefix=${OPENSSL_DST} --openssldir=${OPENSSL_DST}
