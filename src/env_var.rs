d35b42bb6451c964091b72e4092eac4e44fdca70
i-georgiev28 <IvanGeorgiev28@itpg-varna.bg>
2025-07-08 12:47:34 +0300

    Add `foreman setup` command. Add utilities module for working with $PATH.

---
+++ src/env_var.rs
@@ -0,0 +1,61 @@
+use std::{
+    env,
+    fs::OpenOptions,
+    io::{self, Write},
+    path::{Path, PathBuf},
+    process::Command,
+};
+
+pub fn is_in_path<P: AsRef<Path>>(path: P) -> bool {
+    let target = match path.as_ref().canonicalize() {
+        Ok(p) => p,
+        Err(_) => return false,
+    };
+
+    if let Some(paths) = env::var_os("PATH") {
+        for entry in env::split_paths(&paths) {
+            if entry.is_dir() {
+                if let Ok(canon) = entry.canonicalize() {
+                    if canon == target {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    false
+}
+
+pub fn add_to_path<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    _add_to_path(path.as_ref())
+}
+
+#[cfg(target_os = "windows")]
+fn _add_to_path(dir: &Path) -> io::Result<()> {
+    // To modify for all users use `/M` and require admin
+    let dir_str = dir.display().to_string();
+    let value = format!("%PATH%;{}", dir_str);
+    Command::new("setx")
+        .args(&["PATH", &value])
+        .status()
+        .map(|_| ())
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+}
+
+#[cfg(unix)]
+fn _add_to_path(dir: &Path) -> io::Result<()> {
+    // For macOS also paths.d may be modified so I suggest looking more into it. Though it requires elevated permissions
+
+    let shell = env::var("SHELL").unwrap_or_default();
+    let mut rc = PathBuf::from(env::var("HOME").unwrap());
+    if shell.contains("zsh") {
+        rc.push(".zshrc");
+    } else {
+        // Prefer login file with fallback
+        rc.push(".bash_profile");
+    }
+    let mut file = OpenOptions::new().create(true).append(true).open(&rc)?;
+    writeln!(file, "export PATH=\"{}:$PATH\"", dir.display())?;
+    Ok(())
+}
